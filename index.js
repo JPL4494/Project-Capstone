887a4200a40899c9fcbfb5c3d7ffb551e1a5a6a1
JPL4494 <joshlong4494@gmail.com>
2016-12-05 16:07:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -682,7 +682,7 @@ function offenseSelected()
 			cb4.transition()
                             .duration(1000)
                             .delay(10)
-                            .attr("cy", 180);
+                            .attr("cy", 240);
 		
 			fs.transition()
                             .duration(1000)
